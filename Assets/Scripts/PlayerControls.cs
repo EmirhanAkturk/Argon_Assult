4108c5dbd20990a7186478c8ec2633bb56cd7483
emirhanakturk <emirhanakturk@gmail.com>
2021-03-13 09:07:58 +0300

    Player movement has begun.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+   [SerializeField] 
+    [Range(-1f,1f)]  float xOffset = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
 
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);        
+        Vector3 oldPosition = transform.localPosition;
+        float newXPos = oldPosition.x + xOffset;
+
+        transform.localPosition = new  Vector3 (newXPos , oldPosition.y, oldPosition.z);
     }
 }

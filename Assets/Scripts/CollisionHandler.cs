8a2d4737d0dbb318ad658ce047a56ec9fd013680
emirhanakturk <emirhanakturk@gmail.com>
2021-04-23 00:26:34 +0300

    CollisionHandler script has been added.

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(this.name + "--Collided with--" + collision.gameObject.name);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{this.name } **Trigged with** {other.gameObject.name}");
+
+    }
+}

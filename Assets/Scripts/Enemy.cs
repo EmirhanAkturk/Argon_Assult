487690263f3da099bc6580a8d9241d400f2c5a07
emirhanakturk <emirhanakturk@gmail.com>
2021-04-26 01:57:34 +0300

    Scoreboard added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,29 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosionEffect;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject particle;
-        particle = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        ProcessHit();
+        KillEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    private void KillEnemy()
+    {
+        GameObject particle = Instantiate(explosionEffect, transform.position, Quaternion.identity);
         particle.transform.parent = parent;
         Destroy(gameObject, 0.1f);
     }

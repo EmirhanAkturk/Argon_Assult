0f43170d70fd9d688a796e6b4d4603a91802f8e0
emirhanakturk <emirhanakturk@gmail.com>
2021-04-28 17:47:33 +0300

    Enemy ships have been added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Particles")]
     [SerializeField] GameObject explosionEffect;
     [SerializeField] GameObject hitEffect;
 
-    [SerializeField] Transform parent;
-
+    [Header("Hit Parameters")]
     [SerializeField] int health = 30;
-    [SerializeField] int scorePerHit = 10;
-    [SerializeField] int amountOfDamage = 10;
+    [SerializeField] int scorePerHit = 100;
+    [SerializeField] int amountOfDamage = 20;
 
+    Transform parentTr;
     ScoreBoard scoreBoard;
 
     public int GetAmountOfDamage()
@@ -23,30 +24,44 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        AssignParentObject();
+
+        AddRigidBody();
+    }
+
+    void AssignParentObject()
+    {
+        GameObject[] parent = GameObject.FindGameObjectsWithTag("Parent");
+        parentTr = parent[0].transform;
+    }
+
+    void AddRigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
+
+        if (health <= 0)
+            KillEnemy();
     }
 
     private void ProcessHit()
     {
         GameObject particle = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        particle.transform.parent = parent;
+        particle.transform.parent = parentTr;
 
+        scoreBoard.IncreaseScore(scorePerHit);
         health -= 10;
-        if(health <= 0)
-        {
-            scoreBoard.IncreaseScore(scorePerHit);
-            KillEnemy();
-        }
     }
 
     private void KillEnemy()
     {
         GameObject particle = Instantiate(explosionEffect, transform.position, Quaternion.identity);
-        particle.transform.parent = parent;
+        particle.transform.parent = parentTr;
         Destroy(gameObject, 0.1f);
     }
 }
